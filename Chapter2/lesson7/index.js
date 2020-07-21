8632ae10d764a9cdfb62d6b057d68464fa8d013b
uni51 <uni51@users.noreply.github.com>
2020-07-21 11:00:31 +0900

    7. スコープ

---
+++ Chapter2/lesson7/index.js
@@ -0,0 +1,18 @@
+// グローバル変数
+message = "外側";
+
+var test = function () {
+  var message = "関数内";
+  console.log(`1: ${message}`);
+
+  {
+    let message = "ブロック内";
+    console.log(`2: ${message}`);
+  }
+
+  console.log(`3: ${message}`);
+};
+
+test();
+
+console.log(`4: ${message}`);
